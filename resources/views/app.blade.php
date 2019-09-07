aab5732e9ceb90a9dd8e88f5a44db7242b83b29e
Oscar Carrasco <oscar.hcarrasco03@gmail.com>
2019-09-06 22:17:27 -0500

    Some configs
    implementing some libraries for the front end

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">≤
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Grupo Carrasco</title>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    </head>
+    <body>
+        <div id="app"></div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
+    <body>
+</html>
