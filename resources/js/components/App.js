1cf7a4faedd088e7dd341f651608b0f04e79ee88
Oscar Carrasco <oscar.hcarrasco03@gmail.com>
2019-09-06 22:33:30 -0500

    Config React App
    First config to implement react on laravel

---
+++ resources/js/components/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<App />, document.getElementById('app'));
+}
+
