93d32e98719da72f75bef0e152cb2e65d03f30c7
Oscar Carrasco <oscar.hcarrasco03@gmail.com>
2019-09-09 00:30:15 -0500

    Implementing auth

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,7 +1,28 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-if (document.getElementById('app')) {
-    ReactDOM.render(<App />, document.getElementById('app'));
+const App = () => {
+    return (
+        <React.Fragment>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">Example Component</div>
+
+                            <div className="card-body">
+                                I'm an example component!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default App;
+
+if (document.getElementById("app")) {
+    ReactDOM.render(<App />, document.getElementById("app"));
 }
-
